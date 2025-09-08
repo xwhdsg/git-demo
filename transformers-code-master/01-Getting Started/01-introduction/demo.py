53786c19c58a367ebe671c86f546d2567568f5c0
郭铭 <2304725936@qq.com>
2025-09-08 11:30:56 +0800

    second commit

--- transformers-code-master/01-Getting Started/01-introduction/demo.py
+++ transformers-code-master/01-Getting Started/01-introduction/demo.py
@@ -5,3 +5,5 @@ from transformers import pipeline
 # 通过Interface加载pipeline并启动阅读理解服务
 # 如果无法通过这种方式加载，可以采用离线加载的方式
 gr.Interface.from_pipeline(pipeline("question-answering", model="uer/roberta-base-chinese-extractive-qa")).launch()
+
+print("启动")
